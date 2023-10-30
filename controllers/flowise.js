4e98041ccdb3e57a8cf5a16ef857760594944518
gongnone <wjs@williamjshaw.ca>
2023-10-30 15:07:37 -0400

    Flowise_API_Frontend

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 export const createPrediction = async (req, res) => {
   const { message } = req.body;
   console.log(message);
@@ -8,7 +10,23 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    // Call the Flowise Endpoint
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`
+        },
+        body: JSON.stringify(flowiseData),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    res.status(200).json({ message: data });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
